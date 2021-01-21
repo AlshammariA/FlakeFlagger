f00f93e97a6fd7febeaeb7407e1ccb61c4558ea1
AlshammariA <aalsha2@masonlive.gmu.edu>
2021-01-21 22:28:21 +0300

    few updates on IG computation script ..

---
+++ flakiness-predicter/informationGain.sh
@@ -0,0 +1,5 @@
+# this to run generate information gain for given processsed_data
+
+DATA="result/processed_data_with_vocabulary_per_test_for_one_project.csv"
+
+python3 compute_information_gain.py $DATA
